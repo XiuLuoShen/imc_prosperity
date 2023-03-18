78c1f781645f6e2dc5886d7c863fea7a7d843ce0
XiuLuoShen <zengzeng419@gmail.com>
2023-03-18 00:23:21 -0400

    Feature analysis of quote vs. future time

--- trader_mm.py
+++ trader_mm.py
@@ -8,10 +8,45 @@ POSITION_LIMITS = {
     "BANANAS": 20,
 }
 
+FAIR_PRICE = {
+    "PEARLS": 10000
+}
+
 def log_orders(timestamp, orders):
     print("{} SENT_ORDERS {}".format(timestamp, json.dumps(orders, default=lambda o: o.__dict__, sort_keys=True)))
 
     return
+
+def flatten_position(state: TradingState, product, orders: Dict[str, List[Order]]):
+    pos = state.position[product]
+    if pos == 0:
+        return
+    if pos > 0:
+        px = max(state.order_depths[product].buy_orders.keys())
+    elif pos < 0:
+        px = min(state.order_depths[product].sell_orders.keys())
+    orders.append(Order(product, px, -pos))
+
+    return
+
+def alpha_trade(state: TradingState, product, orders: List[Order]):
+    order_depth: OrderDepth = state.order_depths[product]
+    acceptable_price = FAIR_PRICE[product]
+
+    if len(order_depth.sell_orders) > 0:
+        best_ask = min(order_depth.sell_orders.keys())
+        best_ask_volume = order_depth.sell_orders[best_ask]
+
+        if best_ask < acceptable_price:
+            orders.append(Order(product, best_ask, -best_ask_volume))
+
+    if len(order_depth.buy_orders) != 0:
+        best_bid = max(order_depth.buy_orders.keys())
+        best_bid_volume = order_depth.buy_orders[best_bid]
+        if best_bid > acceptable_price:
+            orders.append(Order(product, best_bid, -best_bid_volume))
+
+    return orders
 
 class Trader:
 
@@ -21,26 +56,13 @@ class Trader:
         result = {}
         for product in state.order_depths.keys():
             if product == 'PEARLS':
-                order_depth: OrderDepth = state.order_depths[product]
-
                 orders: list[Order] = []
-                acceptable_price = 10000
-
-                if len(order_depth.sell_orders) > 0:
-                    best_ask = min(order_depth.sell_orders.keys())
-                    best_ask_volume = order_depth.sell_orders[best_ask]
-
-                    if best_ask < acceptable_price:
-
-                        orders.append(Order(product, best_ask, -best_ask_volume))
-
-                if len(order_depth.buy_orders) != 0:
-                    best_bid = max(order_depth.buy_orders.keys())
-                    best_bid_volume = order_depth.buy_orders[best_bid]
-                    if best_bid > acceptable_price:
-                        orders.append(Order(product, best_bid, -best_bid_volume))
-
-                result[product] = orders
+                if state.timestamp < 50000:
+                    alpha_trade(state, product, orders)
+                else:
+                    flatten_position(state, product, orders)
+                if orders:
+                    result[product] = orders
 
         if result:
             log_orders(state.timestamp, result)
